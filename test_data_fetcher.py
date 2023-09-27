0fe88957bff657004c697ded890a59659e2055d5
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-09-27 18:36:01 +0300

    0.3 Add DataFetcher with success test q

---
+++ test_data_fetcher.py
@@ -0,0 +1,15 @@
+import unittest
+from data_fetcher import CustomDataFetcher
+
+class TestCustomDataFetcher(unittest.TestCase):
+    def setUp(self):
+        self.data_fetcher = CustomDataFetcher()
+
+    def test_get_custom_data_from_url(self):
+        custom_data = self.data_fetcher.get_custom_data_from_url("https://example.com", 5)
+        self.assertTrue(isinstance(custom_data, list))
+
+if __name__ == '__main__':
+    unittest.main()
+
+#python3 -m unittest test_data_fetcher.py
