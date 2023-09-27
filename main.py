c3e8d4b27c7b9f7f43767f6d5f27f2b72e35977e
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2023-09-27 18:43:56 +0300

    0.6 Update main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from user_list_generator import CustomUserListGenerator
 from data_fetcher import CustomDataFetcher
+from translator import CustomTranslator
 
 if __name__ == "__main__":
     custom_base_url = "https://sef.podkolzin.consulting/api/users/lastSeen"
@@ -13,5 +14,11 @@ if __name__ == "__main__":
     custom_users = custom_user_list_generator.get_custom_users(custom_data)
     current_time = datetime.utcnow()
 
+    print("Choose language (en, uk, ja, fi): ")
+    custom_lang = input()
+
+    custom_translator = CustomTranslator()
+    custom_user_list_generator.custom_when_online(custom_users, current_time)
+
     for custom_user in custom_users:
-        print(f"{custom_user.username} {(custom_user.last_seen)}")
+        print(f"{custom_user.username} {custom_translator.translate(custom_user.last_seen, custom_lang)}")
